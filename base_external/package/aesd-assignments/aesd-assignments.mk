780445a2a33bae68197ef99344b10d5da8ef41ce
Mich2899 <mich1576@colorado.edu>
2021-09-19 02:47:55 -0600

    after changing makefile and finder-test

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5da364a657917f5050751254f6b00818627e5a7a
+AESD_ASSIGNMENTS_VERSION = 1f5521786a131c499503932d9d1224520094754d
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
